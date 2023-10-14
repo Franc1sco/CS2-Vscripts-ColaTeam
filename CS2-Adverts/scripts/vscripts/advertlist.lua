76c3e06ba11b6a68f3a0ed35bc7d07fdea405f16
Chema <chematerrax@hotmail.es>
2023-10-14 14:45:24 +0200

    Added timers for the adverts. Added cvar adverts_mode 0/1 (timer/rounds). Added cvar show_adverts_timer_delay (control amount of seconds between adverts). Adverts now show 1 by 1 instead of all at once to emulate the behaviour of the csgo plugins we had. Extracted cvars to convars.lua file.

--- CS2-Adverts/scripts/vscripts/advertlist.lua
+++ CS2-Adverts/scripts/vscripts/advertlist.lua
@@ -1,7 +1,7 @@
 -- Add here your adverts on round start
 AdvertList = {
 	"The game is {lightgreen}MULTIMAPS 16000K{white}. You will get {lightgreen}16000K{white} every round",
-	"Add us to your favorites {lightgreen}IP: cs2.plusultra.team{white}",
-    "Join discord {lightgreen}https://discord.gg/ZfeYuNpHA5{white}",
+	"Add us to your favorites {lightgreen}IP: 141.94.98.165:27015{white}",
+    "Join discord {lightgreen}https://discord.gg/czP2W5CVx8{white}",
     "Somos una {lightgreen}Comunidad Española{white}",
 }
